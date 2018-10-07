27c51ba55dde53623c824e462015eadd02d3247c
Kelvin Jones <kevinjones4453@gmail.com>
2018-10-07 16:37:18 -0400

    added image borders r results

--- resources/js/pets.js
+++ resources/js/pets.js
@@ -5,46 +5,68 @@ $('#myModal').on('shown.bs.modal', function () {
 // Define params
 let apiKey = '40902c4189b33d9649efd9725215fcf6';
 let url = 'http://api.petfinder.com/pet.find';
+let input, myPet, zip, myOffset;
 
 $(document).ready(function () {
 
     $('#find').on('click', function () {
         event.preventDefault();
-        let zip = $('#zip').val().trim();
-        let myPet = $('#pets option:selected').text().toLowerCase();
-        console.log(zip);
-        console.log(myPet);
-        getPets(myPet, zip);
+        myOffset = 0;
+        getPets();
+    });
+    
+    $('#more').on('click', function () {
+        event.preventDefault();
+        getPets()
     })
 
 });
 
-function getPets(pet, zip) {
+function getPets() {
+    input = $('#zip').val().trim();
+    myPet = $('#pets option:selected').text().toLowerCase();
+    console.log(input);
+    console.log(myPet);
+
+    if(input.match(/^\d{5}$/)) {
+        zip = input;
+    } else {
+        $('#list').empty();
+        $('#list').append(`
+            <div class="col-sm-12 col-md-6 ">
+            <img src="https://vignette.wikia.nocookie.net/battlefordreamislandfanfiction/images/b/be/Error_Sign_Body_Asset.png/revision/latest?cb=20170426033544" alt="error"><br>
+            <h1>Invalid Zip Code!</h1><br>
+            <h3>Try Again.</h3>
+            </div>`);
+    }
+
     $.ajax({
         url: url,
         jsonp: "callback",
         dataType: "jsonp",
         data: {
             key: apiKey,
-            animal: pet,
+            animal: myPet,
             'location': zip,
+            offset: myOffset,
             output: 'basic',
             format: 'json'
         },// Here is where we handle the response we got back from Petfinder
         success: function( response ) {
-            console.log(response.petfinder.pets.pet); // debugging
+            console.log(response); // debugging
+            myOffset = response.petfinder.lastOffset.$t;
+            console.log(myOffset);
 
             if (response) {
                 if (response.petfinder.pets.pet.length > 0) {
                     displayResults(response.petfinder.pets.pet);
-                } else {
-                    $('#list').append(`<h5>No results matching your criteria. Consider broadening your search.</h5>`);
                 }
-            } else {
-                $('#list').append(`<h5>No results matching your criteria. Consider broadening your search.</h5>`);
             }
 
-
+        },
+        error : function(request, error)
+        {
+            console.log("Request: "+JSON.stringify(request));
         }
     });
 
@@ -63,12 +85,12 @@ function displayResults(response) {
         if (!jQuery.isEmptyObject(record.media)) {
             img = record.media.photos.photo[2].$t;
         } else {
-            img = "https://ubisafe.org/images/nose-vector-dogs.png";
+            img = "https://pawedin.com/system/pets/default_images/default_pet.jpg";
         }
 
         modal.append(`
-            <div id="animals" class="col-md-3">
-                <img src="${img}" width="150" height="150" alt="${petName}"><br>
+            <div id="animals" class="col-sm-12 col-md-3">
+                <img src="${img}"  id="petImg" width="150" height="150" alt="${petName}"><br>
                 <span>Name: <a href="https://www.petfinder.com/petdetail/${id}">${petName}</a></span><br>
                 <span>Breed: ${breed}</span><br>
                 <span>Age: ${age}</span>
