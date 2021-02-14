836bc6908f6a2be1fd4bfb59cfe9f3532241484d
Kelvin Jones <kevinjones4453@gmail.com>
2021-02-14 13:38:34 -0500

    Updated the code to use petfinder v2 api

--- resources/js/pets.js
+++ resources/js/pets.js
@@ -10,6 +10,16 @@ window.addEventListener("scroll", function() {
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
 });
 
+// Define params
+const secretId = 'mEQQjdPJguNqITS2J6vonPDwQfciH9elGRTluER3'
+const apiKey = 'H8TDMo0avTLwWzx10WMPcoDZDyh234p5cMz6EbXVssyIeQz79o';
+const url = 'https://api.petfinder.com/v2/';
+let petFinder;
+let input, myPet, zip, page;
+let tempBreed = '';
+let computerPick, factsTimeout;
+let factCounter = 0;
+
 let facts = [
     "Cats have over one hundred vocal sounds, while dogs only have about ten.",
     "A bird's heart beats 400 times per minute while they are resting.",
@@ -30,38 +40,51 @@ let facts = [
     "Dogs have about 10 vocal sounds."
 ];
 
-// Define params
-const apiKey = '40902c4189b33d9649efd9725215fcf6';
-const url = 'https://api.petfinder.com/pet.find';
-let input, myPet, zip, myOffset;
-let tempBreed = '';
-let computerPick, factsTimeout;
-let factCounter = 0;
-
 $(document).ready(function () {
+    // Init Pet Finder
+    petFinder = new petfinder.Client({ apiKey: apiKey, secret: secretId });
+
+    // Get Animal Types and populate the drop-down
+    petFinder.animalData.types()
+        .then((res, rej) => {
+            res.data.types.map(type => type.name)
+                .forEach(name => {
+                    $('#pet-types').append(`
+                        <option value=${name}>${name}</option>
+                    `);
+                });
+        })
+        .catch(err => console.log(err));
 
     $('#find').on('click', function () {
         event.preventDefault();
-        myOffset = 0;                       // reset the pagination
+        page = 1;                   // reset the pagination
         input = $('#zip').val().trim();
 
-        if(input.match(/^\d{5}$/)) {        // input validation
+        if(input.match(/^\d{5}$/)) {     // input validation
             zip = input;
             $('#more').css('display', 'block'); // enable the more button
             getPets();
         } else {
             $('#list').empty();
             $('#list').append(`
-            <div class="col-sm-12 col-md-6 text-center">
-            <img class="img-fluid" src="https://vignette.wikia.nocookie.net/battlefordreamislandfanfiction/images/b/be/Error_Sign_Body_Asset.png/revision/latest?cb=20170426033544" alt="error"><br>
-            <h1>Invalid Zip Code!</h1><br>
-            <h3>Try Again.</h3>
-            </div>`);
+                <div class="col-sm-12 col-md-6 text-center">
+                <img class="img-fluid" src="https://vignette.wikia.nocookie.net/battlefordreamislandfanfiction/images/b/be/Error_Sign_Body_Asset.png/revision/latest?cb=20170426033544" alt="error"><br>
+                <h1>Invalid Zip Code!</h1><br>
+                <h3>Try Again.</h3>
+                </div>`
+            );
         }
     });
     
     $('#more').on('click', function () {
         event.preventDefault();
+        page++;
+        getPets();
+    });
+    $('#back').on('click', function () {
+        event.preventDefault();
+        if (page > 1) page--;
         getPets();
     });
 
@@ -77,75 +100,69 @@ $(document).ready(function () {
     })
 });
 
-function getPets() {
-    myPet = $('#pets option:selected').text().toLowerCase();
+async function getPets() {
 
-    $.ajax({
-        url: url,
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {
-            key: apiKey,
-            animal: myPet,
-            'location': zip,
-            offset: myOffset,
-            output: 'basic',
-            format: 'json'
-        },// Here is where we handle the response we got back from Petfinder
-        success: function( response ) {
-            console.log(response); // debugging
-            myOffset = response.petfinder.lastOffset.$t;        // save the lastOffset value in case user wants more results
-            console.log(myOffset);
+    const petType = $('#pet-types option:selected').text().trim();
+    let searchRes;
+    try {
+        searchRes = await petFinder.animal.search({
+            type: petType,
+            location: zip,
+            distance: 25,
+            page: page,
+            limit: 20
+        });
+    } catch (err) {
+        console.log(err);
+        $('#list').empty();
+        $('#list').append(`
+            <div class="col-sm-12 col-md-6 text-center">
+            <p>An Error has occurred. :(</p>
+            <p>Try Again.</p>
+            </div>`);
+    }
 
-            if (response) {
-                if (response.petfinder.pets.pet.length > 0) {
-                    displayResults(response.petfinder.pets.pet);
-                }
-            }
-
-        },
-        error : function(request, error)
-        {
-            console.log("Request: "+JSON.stringify(request));
-            $('#list').empty();
-            $('#list').append(`
-                <div class="col-sm-12 col-md-6 text-center">
-                <p>An Error has occurred. :(</p><br>
-                <p>Try Again.</p>
-                </div>`);
-        }
-    });
-
+    if (searchRes && searchRes.data.animals.length > 0) {
+        displayResults(searchRes.data.animals);
+    } else if (searchRes &&
+        (searchRes.data.total_count === 0 || searchRes.data.animals.length === 0)) {
+        $('#list').empty();
+        $('#list').append(`
+            <div class="col-sm-12 col-md-6 text-center">
+            <p>No results found :(</p>
+            <p>Try a different search</p>
+            </div>`);
+    }
 }
 
 function displayResults(response) {
+
     let modal = $('#list');
     modal.empty();                          // empty the contents of the div
     response.forEach(function (record) {
-        let petName = record.name.$t;
-        let id = record.id.$t;
+        const petName = record.name;
+        const id = record.id;
         let breed;
-        //let breed = record.breeds.breed.$t;
-        let age = record.age.$t;
+        let age = record.age;
         let img;
 
         // test for images
-        if (!jQuery.isEmptyObject(record.media)) {
-            img = record.media.photos.photo[2].$t;
+        if (!jQuery.isEmptyObject(record.photos)) {
+            img = record.photos[0].medium;
         } else {
             img = "https://pawedin.com/system/pets/default_images/default_pet.jpg";
         }
 
-        if(!jQuery.isEmptyObject(record.breeds.breed)) {
+        if(!jQuery.isEmptyObject(record.breeds)) {
             // in the case of multiple breeds
-            if(Array.isArray(record.breeds.breed)) {                // check to see if the object is an array
-                record.breeds.breed.forEach(function (arrayItem) {
-                    tempBreed += (arrayItem.$t + '');               // concatenate the breed names
+            if(Array.isArray(record.breeds)) {                // check to see if the object is an array
+                record.breeds.forEach(function (arrayItem) {
+                    tempBreed += (arrayItem.primary + arrayItem.secondary + '');               // concatenate the breed names
                 });
                 breed = tempBreed.trim();
                 tempBreed = '';
             } else {
-                breed = record.breeds.breed.$t;
+                breed = record.breeds.primary;
             }
         }
 
@@ -163,6 +180,7 @@ function displayResults(response) {
 }
 
 function getRandomFact() {
+
     clearInterval(factsTimeout);            // clear the interval from last time
     computerPick = facts[Math.floor(Math.random() * facts.length)];            // select a random fact
     let factDiv = $(".factIs");
